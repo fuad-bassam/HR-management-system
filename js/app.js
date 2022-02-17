d6f4357ac290e5c2b11833afe7accfa36d6a9a1f
Fuad Abuawad <74737369+fuad-bassam@users.noreply.github.com>
2022-02-17 17:50:59 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,18 +15,15 @@ function employeeInformation(employeeID, fullName, department, level, personalIm
 }
 
 
-
-
-
 employeeInformation.prototype.salaryNumber = function () {
     var min, max, salaryWithoutTax;
-    if (this.level == "junior") {
+    if (this.level == "Junior") {
         min = 500;
         max = 1000;
-    } else if (this.level == "mid-senior") {
+    } else if (this.level == "Mid-Senior") {
         min = 1000;
         max = 1500;
-    } else if (this.level == "senior") {
+    } else if (this.level == "Senior") {
         min = 1500;
         max = 2000;
     } else {
@@ -41,51 +38,169 @@ employeeInformation.prototype.salaryNumber = function () {
 
 }
 
-let Ghazi_Samer = new employeeInformation(1000, "Ghazi Samer", "Administration ", "senior", "URL");
-let Lana_Ali = new employeeInformation(1001, "Lana Ali", "Finance ", "senior", "URL");
-let Tamara_Ayoub = new employeeInformation(1002, "Tamara Ayoub", "Marketing ", "senior", "URL");
-let Safi_Walid = new employeeInformation(1002, "Safi Walid", "Administration ", "mid-senior", "URL");
-let Omar_Zaid = new employeeInformation(1004, "Omar Zaid", "Development ", "senior", "URL");
-let Rana_Saleh = new employeeInformation(1005, "Rana Saleh", "Development ", "junior", "URL");
-let Hadi_Ahmad = new employeeInformation(1006, "Hadi Ahmad", "Finance ", "mid-senior", "URL");
 
-// Ghazi_Samer.
-
-let main1 = document.getElementById("mainBody");
-// main1.textContent = "dddddddd";
-employeeInformation.prototype.render1 = function () {
+// let Ghazi_Samer = new employeeInformation (randId() ,"Ghazi Samer" , "Administration ", "senior" ,"URL");
+// let Lana_Ali = new employeeInformation (randId() ,"Lana Ali" , "Finance ", "senior" ,"URL");
+// let Tamara_Ayoub = new employeeInformation (randId() ,"Tamara Ayoub" , "Marketing ", "senior" ,"URL");
+// let Safi_Walid = new employeeInformation (1002 ,"Safi Walid" , "Administration ", "mid-senior" ,"URL");
+// let Omar_Zaid = new employeeInformation (1004 ,"Omar Zaid" , "Development ", "senior" ,"URL");
+// let Rana_Saleh = new employeeInformation (1005 ,"Rana Saleh" , "Development ", "junior" ,"URL");
+// let Hadi_Ahmad = new employeeInformation (1006 ,"Hadi Ahmad" , "Finance ", "mid-senior" ,"URL");
 
 
-    // document.write(`<h2 style ="margin-bottom : 0px ; color: rgb(62, 6, 114) ; ">${this.fullName}: ${this.salary}</h2>`);
+let main1 = document.getElementById("output");
 
-    // listOfEmployee[x].salaryNumber();
-    // listOfEmployee[x].render();
+// from task 8
+employeeInformation.prototype.render2 = function () {
+    // console.log(this)
+this.salaryNumber();
+this.employeeID= randId(this);
+this.showEmpData();
 
-    let employeeName = document.createElement("h3")
-    let employeeSalary = document.createElement("h4")
+}
+// // from task 7
+// // employeeInformation.prototype.render1 = function () {
 
-    employeeName.textContent = this.fullName
-    employeeSalary.textContent = this.salary
 
-    main1.appendChild(employeeName);
-    main1.appendChild(employeeSalary);
-    main1.appendChild(document.createElement("br"));
+// //     // document.write(`<h2 style ="margin-bottom : 0px ; color: rgb(62, 6, 114) ; ">${this.fullName}: ${this.salary}</h2>`);
+
+// //     // listOfEmployee[x].salaryNumber();
+// //     // listOfEmployee[x].render();
+
+// //     let employeeName = document.createElement("h2")
+// //     let employeeSalary = document.createElement("h3")
+
+// //     employeeName.textContent = this.fullName
+// //     employeeSalary.textContent = this.salary
+
+// //     main1.appendChild(employeeName);
+// //     main1.appendChild(employeeSalary);
+
+// // }
+
+
+
+
+//     // this.salaryNumber();
+//     // this.randId();
+//     // this.showCard();
+//     console.log("here")
+//     console.log(listOfEmployee)
+// };
+
+
+// for (let x = 0; x < listOfEmployee.length; x++) {
+//     console.log(listOfEmployee[x]);
+
+
+//     listOfEmployee[x].salaryNumber();
+//     listOfEmployee[x].render1();
+//     // console.log(listOfEmployee[x]);
+// }
+
+
+function randId(x) {
+    let id,subId;
+
+ console.log(x.department)
+
+    if (x.department == "Administration") {
+       subId = 1;
+    } else if (x.department == "Marketing") {
+       subId = 2;
+    } else if (x.department == "Development") {
+        subId = 3 ;
+    } else if (x.department =="Finance"){
+        subId = 4;
+
+    }
+
+    
+
+    var max = 999
+    var min = 100
+    id = Math.floor(Math.random() * (max - min)) + min;
+ id = subId +""+ id ;
+    // checkId
+    for (let xs = 0; xs < listOfEmployee.length; xs++) {
+        
+        if (id == listOfEmployee[xs].employeeID) {
+            randId()
+            // console.log("sdsdd");
+         
+        }
+    }
+   return id;
+}
+
+
+document.getElementById("form").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    addEmployeeData(event);
+})
+
+function addEmployeeData(event) {
+
+
+            
+    let fullName1 =  event.target.full_Name.value;
+    let department1 =  event.target.department.value;
+    let level =   event.target.level.value;
+    let personalImageURL =   event.target.imag.value;
+
+
+
+    // console.log(fullName1)
+
+    let newEmp = new employeeInformation(0, fullName1, department1, level, personalImageURL)
+    newEmp.render2();
+    // console.log("bass");
 }
 
 
 
-// let main1=document.getElementsByTagName("main") 
-//  console.log(listOfEmployee);
-for (let x = 0; x < listOfEmployee.length; x++) {
-    console.log(listOfEmployee[x]);
+employeeInformation.prototype.showEmpData = function () {
 
-    //         // document.write(`<h2 style ="margin-bottom : 0px ; color: rgb(62, 6, 114) ; ">${this.fullName}: ${this.salary}</h2>`);
-    //         listOfEmployee[x].getSalary();
-    //         listOfEmployee[x].render();
-    // };
 
-    listOfEmployee[x].salaryNumber();
-    listOfEmployee[x].render1();
-    console.log(listOfEmployee[x]);
+
+    let divToShow = document.createElement("div");
+    // divToShow.id=this.id;
+    divToShow.className ="divToShow"
+  
+
+   
+// console.log(this.personalImageURL);
+    let imageUrl = document.createElement("img");
+
+    imageUrl.setAttribute("src", this.personalImageURL);
+
+    imageUrl.className ="imgDivToShow";
+    
+    
+    divToShow.appendChild(imageUrl);
+
+    let empName = document.createElement("h3");
+    empName.textContent = "Name :" + this.fullName;
+    divToShow.appendChild(empName);
+
+    let empId = document.createElement("h3");
+    empId.textContent = "ID :" + this.employeeID;
+    divToShow.appendChild(empId);
+
+    let empDepartment = document.createElement("h3");
+    empDepartment.textContent = "Department :" + this.department;
+    divToShow.appendChild(empDepartment);
+
+    let empLevel = document.createElement("h3");
+    empLevel.textContent = "level :" + this.level;
+    divToShow.appendChild(empLevel);
+
+    let empSalary = document.createElement("h3");
+    empSalary.textContent = "Slary :" + this.salary;
+    divToShow.appendChild(empSalary);
+
+
+    main1.appendChild(divToShow);
+
 }
-
