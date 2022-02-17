e023529f54736c9259aa4bef33ae80ab4a279dc2
fuad-bassam <f.man.x99@gmail.com>
2022-02-17 17:24:55 +0200

    v events

--- js/app.js
+++ js/app.js
@@ -38,6 +38,7 @@ employeeInformation.prototype.salaryNumber = function () {
 
 }
 
+
 // let Ghazi_Samer = new employeeInformation (randId() ,"Ghazi Samer" , "Administration ", "senior" ,"URL");
 // let Lana_Ali = new employeeInformation (randId() ,"Lana Ali" , "Finance ", "senior" ,"URL");
 // let Tamara_Ayoub = new employeeInformation (randId() ,"Tamara Ayoub" , "Marketing ", "senior" ,"URL");
@@ -46,97 +47,160 @@ employeeInformation.prototype.salaryNumber = function () {
 // let Rana_Saleh = new employeeInformation (1005 ,"Rana Saleh" , "Development ", "junior" ,"URL");
 // let Hadi_Ahmad = new employeeInformation (1006 ,"Hadi Ahmad" , "Finance ", "mid-senior" ,"URL");
 
-// Ghazi_Samer.
 
-let main1 = document.getElementById("mainBody");
-// from task 7
-employeeInformation.prototype.render1 = function () {
-
-
-    // document.write(`<h2 style ="margin-bottom : 0px ; color: rgb(62, 6, 114) ; ">${this.fullName}: ${this.salary}</h2>`);
-
-    // listOfEmployee[x].salaryNumber();
-    // listOfEmployee[x].render();
-
-    let employeeName = document.createElement("h2")
-    let employeeSalary = document.createElement("h3")
-
-    employeeName.textContent = this.fullName
-    employeeSalary.textContent = this.salary
-
-    main1.appendChild(employeeName);
-    main1.appendChild(employeeSalary);
-
-}
+let main1 = document.getElementById("output");
 
 // from task 8
 employeeInformation.prototype.render2 = function () {
+    // console.log(this)
+this.salaryNumber();
+this.employeeID= randId(this);
+this.showEmpData();
 
-
-    // this.salaryNumber();
-    // this.randId();
-    // this.showCard();
-console.log("here")
-console.log(listOfEmployee)
-  };
-
-
-for (let x = 0; x < listOfEmployee.length; x++) {
-    console.log(listOfEmployee[x]);
-
-    //         // document.write(`<h2 style ="margin-bottom : 0px ; color: rgb(62, 6, 114) ; ">${this.fullName}: ${this.salary}</h2>`);
-    // };
-
-    listOfEmployee[x].salaryNumber();
-    listOfEmployee[x].render1();
-    console.log(listOfEmployee[x]);
 }
- function randId(){
-let id;
-var max =9999
-var min=1000
-id =Math.floor(Math.random() * (max - min)) + min;
-checkId(id);
-return id;
-}
-
-function checkId(id){
+// // from task 7
+// // employeeInformation.prototype.render1 = function () {
 
 
-    for(let xs=0; xs<listOfEmployee.length ;xs++){
-// if(listOfEmployee.length != 0){
+// //     // document.write(`<h2 style ="margin-bottom : 0px ; color: rgb(62, 6, 114) ; ">${this.fullName}: ${this.salary}</h2>`);
 
-        if ( id == listOfEmployee[xs].employeeID ){
+// //     // listOfEmployee[x].salaryNumber();
+// //     // listOfEmployee[x].render();
+
+// //     let employeeName = document.createElement("h2")
+// //     let employeeSalary = document.createElement("h3")
+
+// //     employeeName.textContent = this.fullName
+// //     employeeSalary.textContent = this.salary
+
+// //     main1.appendChild(employeeName);
+// //     main1.appendChild(employeeSalary);
+
+// // }
+
+
+
+
+//     // this.salaryNumber();
+//     // this.randId();
+//     // this.showCard();
+//     console.log("here")
+//     console.log(listOfEmployee)
+// };
+
+
+// for (let x = 0; x < listOfEmployee.length; x++) {
+//     console.log(listOfEmployee[x]);
+
+
+//     listOfEmployee[x].salaryNumber();
+//     listOfEmployee[x].render1();
+//     // console.log(listOfEmployee[x]);
+// }
+
+
+function randId(x) {
+    let id,subId;
+
+ console.log(x.department)
+
+    if (x.department == "Administration") {
+       subId = 1;
+    } else if (x.department == "Marketing") {
+       subId = 2;
+    } else if (x.department == "Development") {
+        subId = 3 ;
+    } else if (x.department =="Finance"){
+        subId = 4;
+
+    }
+
+    
+
+    var max = 999
+    var min = 100
+    id = Math.floor(Math.random() * (max - min)) + min;
+ id = subId +""+ id ;
+    // checkId
+    for (let xs = 0; xs < listOfEmployee.length; xs++) {
+        
+        if (id == listOfEmployee[xs].employeeID) {
             randId()
-                console.log("sdsdd");
-                break;
+            // console.log("sdsdd");
+         
         }
     }
-// }
+   return id;
 }
-document.getElementById("submitt").addEventListener("click",function(event){
+
+
+document.getElementById("form").addEventListener("submit", function (event) {
     event.preventDefault();
 
-   addEmployeeData ();
+    addEmployeeData(event);
 })
 
-function addEmployeeData(){
-// employeeID
+function addEmployeeData(event) {
+
+
+            
+    let fullName1 =  event.target.full_Name.value;
+    let department1 =  event.target.department.value;
+    let level =   event.target.level.value;
+    let personalImageURL =   event.target.imag.value;
 
 
 
-let fullName1 ='default' ;
-let department1= 'Development' ;
- let level ='junior' ;
- let personalImageURL='https://thumbs.dreamstime.com/b/creative-vector-illustration-default-avatar-profile-placeholder-isolated-background-art-design-grey-photo-blank-template-mo-107388687.jpg'
+    // console.log(fullName1)
 
-fullName1 = document.getElementById("full_Name") ;
-department1 =document.getElementById("department");
-level = document.getElementById("level");
- personalImageURL= document.getElementById("imag") ;
+    let newEmp = new employeeInformation(0, fullName1, department1, level, personalImageURL)
+    newEmp.render2();
+    // console.log("bass");
+}
 
-fullName1.op
- let newEmp= new employeeInformation(randId,fullName1,department1,level,personalImageURL)
- newEmp.render2 ();
- console.log("bass");
+
+
+employeeInformation.prototype.showEmpData = function () {
+
+
+
+    let divToShow = document.createElement("div");
+    // divToShow.id=this.id;
+    divToShow.className ="divToShow"
+  
+
+   
+// console.log(this.personalImageURL);
+    let imageUrl = document.createElement("img");
+
+    imageUrl.setAttribute("src", this.personalImageURL);
+
+    imageUrl.className ="imgDivToShow";
+    
+    
+    divToShow.appendChild(imageUrl);
+
+    let empName = document.createElement("h3");
+    empName.textContent = "Name :" + this.fullName;
+    divToShow.appendChild(empName);
+
+    let empId = document.createElement("h3");
+    empId.textContent = "ID :" + this.employeeID;
+    divToShow.appendChild(empId);
+
+    let empDepartment = document.createElement("h3");
+    empDepartment.textContent = "Department :" + this.department;
+    divToShow.appendChild(empDepartment);
+
+    let empLevel = document.createElement("h3");
+    empLevel.textContent = "level :" + this.level;
+    divToShow.appendChild(empLevel);
+
+    let empSalary = document.createElement("h3");
+    empSalary.textContent = "Slary :" + this.salary;
+    divToShow.appendChild(empSalary);
+
+
+    main1.appendChild(divToShow);
+
 }
